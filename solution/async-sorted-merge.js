503f243641ee2ed99bf414ed05ab996a223953e0
Chetu Khandavilli <skchetu.k@gmail.com>
2023-10-22 01:17:44 -0700

    Add comments for clarity

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -7,6 +7,7 @@ const MinHeap = require('../lib/log-min-heap');
 module.exports = async (logSources, printer) => {
   const minHeap = new MinHeap();
 
+  // Pop and add log entry the min-heap.
   const popLogEntryAsync = async (logSource, sourceIndex) => {
     const nextLogEntry = await logSource.popAsync();
 
@@ -15,15 +16,18 @@ module.exports = async (logSources, printer) => {
     }
   };
 
+  // Add earliest log entry from each logSource to min-heap.
   await Promise.all(
     logSources.map((logSource, index) => popLogEntryAsync(logSource, index))
   );
 
   while (!minHeap.isEmpty()) {
+    // Print next earlist log entry.
     const { entry, sourceIndex } = minHeap.getEarliestLogEntry();
-
     printer.print(entry);
 
+    // Adds next consecutive log entry from same logSource.
+    // Maintains efficiency by preventing the log entry min-heap from becoming too large.
     await popLogEntryAsync(logSources[sourceIndex], sourceIndex);
   }
 
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,28 +1,31 @@
 'use strict';
 
-const { first } = require('lodash');
-const MinHeap = require('../lib/log-min-heap');
+const LogMinHeap = require('../lib/log-min-heap');
 
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  const minHeap = new MinHeap();
+  const logMinHeap = new LogMinHeap();
 
+  // Pop and add log entry the min-heap.
   const popLogEntry = (logSource, sourceIndex) => {
     const nextLogEntry = logSource.pop();
 
     if (!logSource.drained) {
-      minHeap.insert({ entry: nextLogEntry, sourceIndex });
+      logMinHeap.insert({ entry: nextLogEntry, sourceIndex });
     }
   };
 
+  // Add earliest log entry from each logSource to min-heap.
   logSources.map((logSource, index) => popLogEntry(logSource, index));
 
-  while (!minHeap.isEmpty()) {
-    const { entry, sourceIndex } = minHeap.getEarliestLogEntry();
-
+  while (!logMinHeap.isEmpty()) {
+    // Print next earlist log entry.
+    const { entry, sourceIndex } = logMinHeap.getEarliestLogEntry();
     printer.print(entry);
 
+    // Adds next consecutive log entry from same logSource.
+    // Maintains efficiency by preventing the log entry min-heap from becoming too large.
     popLogEntry(logSources[sourceIndex], sourceIndex);
   }
 
