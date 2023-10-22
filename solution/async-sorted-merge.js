aa37548f6bd703444a0092cf0ca04d0717a0513b
Chetu Khandavilli <skchetu.k@gmail.com>
2023-10-22 00:14:27 -0700

    Add implementations of sync and async merges

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,33 @@
-"use strict";
+'use strict';
+
+const MinHeap = require('../lib/log-min-heap');
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
-  });
+module.exports = async (logSources, printer) => {
+  const minHeap = new MinHeap();
+
+  const popLogEntryAsync = async (logSource, sourceIndex) => {
+    const nextLogEntry = await logSource.popAsync();
+
+    if (!logSource.drained) {
+      minHeap.insert({ entry: nextLogEntry, sourceIndex });
+    }
+  };
+
+  await Promise.all(
+    logSources.map((logSource, index) => popLogEntryAsync(logSource, index))
+  );
+
+  while (!minHeap.isEmpty()) {
+    const { entry, sourceIndex } = minHeap.getEarliestLogEntry();
+
+    printer.print(entry);
+
+    await popLogEntryAsync(logSources[sourceIndex], sourceIndex);
+  }
+
+  printer.done();
+
+  return console.log('Async sort complete.');
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,32 @@
-"use strict";
+'use strict';
+
+const { first } = require('lodash');
+const MinHeap = require('../lib/log-min-heap');
 
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+  const minHeap = new MinHeap();
+
+  const popLogEntry = (logSource, sourceIndex) => {
+    const nextLogEntry = logSource.pop();
+
+    if (!logSource.drained) {
+      minHeap.insert({ entry: nextLogEntry, sourceIndex });
+    }
+  };
+
+  logSources.map((logSource, index) => popLogEntry(logSource, index));
+
+  while (!minHeap.isEmpty()) {
+    const { entry, sourceIndex } = minHeap.getEarliestLogEntry();
+
+    printer.print(entry);
+
+    popLogEntry(logSources[sourceIndex], sourceIndex);
+  }
+
+  printer.done();
+
+  return console.log('Sync sort complete.');
 };
