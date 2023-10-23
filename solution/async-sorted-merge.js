8a8a694dd4d3b2a72b6c620a93ae341d2bfb731c
Chetu Khandavilli <skchetu.k@gmail.com>
2023-10-23 13:40:30 -0700

    Update async solution to continuously fetch log entries while limiting the heap size

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,34 +1,46 @@
 'use strict';
 
-const MinHeap = require('../lib/log-min-heap');
+const LogMinHeap = require('../lib/log-min-heap');
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
 module.exports = async (logSources, printer) => {
-  const minHeap = new MinHeap();
+  const logMinHeap = new LogMinHeap();
+
+  // Define the maximum size of the min-heap.
+  const MAX_HEAP_SIZE = 1000; // Adjust this value as needed.
 
   // Pop and add log entry the min-heap.
-  const popLogEntryAsync = async (logSource, sourceIndex) => {
-    const nextLogEntry = await logSource.popAsync();
+  const popLogEntriesAsync = async (logSource, sourceIndex) => {
+    while (!logSource.drained) {
+      const nextLogEntry = await logSource.popAsync();
 
-    if (!logSource.drained) {
-      minHeap.insert({ entry: nextLogEntry, sourceIndex });
+      if (!logSource.drained) {
+        logMinHeap.insert({ entry: nextLogEntry, sourceIndex });
+
+        // Max size must be larger than the number of log sources.
+        if (
+          MAX_HEAP_SIZE > logSources.length &&
+          logMinHeap.size() > MAX_HEAP_SIZE
+        ) {
+          return;
+        }
+      }
     }
   };
 
-  // Add earliest log entry from each logSource to min-heap.
+  // Add log entries from each logSource to min-heap until max size is reached.
   await Promise.all(
-    logSources.map((logSource, index) => popLogEntryAsync(logSource, index))
+    logSources.map((logSource, index) => popLogEntriesAsync(logSource, index))
   );
 
-  while (!minHeap.isEmpty()) {
+  while (!logMinHeap.isEmpty()) {
     // Print next earlist log entry.
-    const { entry, sourceIndex } = minHeap.getEarliestLogEntry();
+    const { entry, sourceIndex } = logMinHeap.getEarliestLogEntry();
     printer.print(entry);
 
-    // Adds next consecutive log entry from same logSource.
-    // Maintains efficiency by preventing the log entry min-heap from becoming too large.
-    await popLogEntryAsync(logSources[sourceIndex], sourceIndex);
+    // Adds log entries from same logSource to min-heap until max is reached.
+    await popLogEntriesAsync(logSources[sourceIndex], sourceIndex);
   }
 
   printer.done();
